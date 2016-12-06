1fd9180b02dea4d06a82fa7d90a74136e1cb3827
Jelmer Rotteveel <jelmerrotteveel11@gmail.com>
2016-12-06 22:01:15 +0100

    fade slower into class, little text in about

--- java.js
+++ java.js
@@ -5,23 +5,23 @@ var main = function() {
     });
     
 //Jumbotron
-    $("#fade").addClass("load");
+    $(".fade").addClass("load");
     
     setTimeout(function() {
-        $("#fade2").delay(1000).addClass("load");
-    }, 2000);
+        $(".fade2").delay(1000).addClass("load");
+    }, 1500);
     
     setTimeout(function() {
-        $("#fade3").delay(1200).addClass("load");
-    }, 4000);
+        $(".fade3").delay(1200).addClass("load");
+    }, 3000);
     
     setTimeout(function() {
         $(".btn-fade").delay(1400).addClass("load");  
-    }, 6000); 
+    }, 4000); 
     
     setTimeout(function() {
         $(".btn-fade2").delay(1500).addClass("load");  
-    }, 6000);
+    }, 4000);
     
     };
 
